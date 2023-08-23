5d781f730e3cba7d7e4629d7a6d22fe15a50b83a
GabrielaFeddersen <gabrielafeddersen@gmail.com>
2023-08-22 22:07:55 -0300

    Realização ex 8

--- ex8_conversao_de_medidas.py
+++ ex8_conversao_de_medidas.py
@@ -1,2 +1,7 @@
 # Escreva um programa que leia um valor em metro e faça a coversao para centimetros e milimetros
 
+metros = float(input('Digite a metragem: '))
+cm = metros * 100
+ml = metros *1000
+print(f'{metros:.2f} metros possui {cm:.2f} centimentros')
+print(f'{metros:.2f} metros possui {ml:.2f} melimetros')
